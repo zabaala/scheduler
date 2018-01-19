feaba872f651eba42f00d580eaf05a8a3de31645
Mauricio Rodrigues <mmauricio.vsr@gmail.com>
2018-01-19 16:15:15 -0300

    new learning: abstract class dont need to be tested :)

--- tests/Scheduler/BaseTest.php
+++ tests/Scheduler/BaseTest.php
@@ -7,7 +7,7 @@ use JansenFelipe\FakerBR\FakerBR;
 use Tests\TestCase;
 use Faker\Factory as Faker;
 
-class BaseTest extends TestCase
+abstract class BaseTest extends TestCase
 {
     use RefreshDatabase;
 
@@ -24,13 +24,5 @@ class BaseTest extends TestCase
 
         $this->faker = Faker::create();
         $this->faker->addProvider(new FakerBR($this->faker));
-    }
-
-    /**
-     * like a boss... =D
-     */
-    public function testOk()
-    {
-        $this->assertTrue(true);
     }
 }
