90ff6dff2ae7e4cd562b007a58422c58cbed3523
Mauricio Rodrigues <mmauricio.vsr@gmail.com>
2018-01-19 10:49:19 -0300

    move basic structure to Core folder

--- app/Core/Providers/AuthServiceProvider.php
+++ app/Core/Providers/AuthServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace App\Core\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        'App\Core\Model' => 'App\Core\Policies\ModelPolicy',
     ];
 
     /**
