302f57135a67a17764422d06aae6b1753b5a6c72
Mauricio Rodrigues <mmauricio.vsr@gmail.com>
2018-01-22 15:39:01 -0300

    use cpf class to validate data

--- app/Support/Validation/custom/CpfValidator.php
+++ app/Support/Validation/custom/CpfValidator.php
@@ -7,45 +7,15 @@ use Illuminate\Validation\Validator;
 
 class CpfValidator
 {
-
+    /**
+     * @param $attribute
+     * @param $value
+     * @param $params
+     * @param Validator $validator
+     * @return bool
+     */
     public function validate($attribute, $value, $params, Validator $validator)
     {
-        $cpf = $value;
-
-        $cpf = str_pad(preg_replace('/[^0-9]/ism', '', $cpf), 11, '0', STR_PAD_LEFT);
-
-        // Verifica se nenhuma das sequências abaixo foi digitada, caso seja, retorna falso
-        if (
-            strlen($cpf) != 11
-            || $cpf == '00000000000'
-            || $cpf == '11111111111'
-            || $cpf == '22222222222'
-            || $cpf == '33333333333'
-            || $cpf == '44444444444'
-            || $cpf == '55555555555'
-            || $cpf == '66666666666'
-            || $cpf == '77777777777'
-            || $cpf == '88888888888'
-            || $cpf == '99999999999'
-        )
-        {
-            return false;
-        }
-        else
-        {   // Calcula os números para verificar se o CPF é verdadeiro
-            for ($t = 9; $t < 11; $t++) {
-                for ($d = 0, $c = 0; $c < $t; $c++) {
-                    $d += $cpf{$c} * (($t + 1) - $c);
-                }
-
-                $d = ((10 * $d) % 11) % 10;
-
-                if ($cpf{$c} != $d) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
+        return (new Cpf($value))->validate();
     }
 }
