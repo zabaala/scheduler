db64c5a1d6345f5ab4e46b2a66f25ff6a1bd7618
Mauricio Rodrigues <mmauricio.vsr@gmail.com>
2018-01-23 12:48:55 -0300

    create doctors resource

---
+++ app/Applications/Api/V1/Http/Controllers/DoctorsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Applications\Api\V1\Http\Controllers;
+
+use App\Core\Http\Controllers\Controller;
+use App\Domains\Doctor\DbDoctorRepository;
+use App\Domains\Doctor\Doctor;
+
+class DoctorsController extends Controller
+{
+    /**
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function index()
+    {
+        return (new DbDoctorRepository(new Doctor()))->getAll();
+    }
+}
