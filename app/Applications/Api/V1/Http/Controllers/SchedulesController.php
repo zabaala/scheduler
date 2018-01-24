499f5669d2a7c09206599aab8e83f5cabc1a5dd2
Mauricio Rodrigues <mmauricio.vsr@gmail.com>
2018-01-24 08:28:52 -0300

    inject DbScheduleRepository via DI

--- app/Applications/Api/V1/Http/Controllers/SchedulesController.php
+++ app/Applications/Api/V1/Http/Controllers/SchedulesController.php
@@ -3,16 +3,16 @@
 namespace App\Applications\Api\V1\Http\Controllers;
 
 use App\Core\Http\Controllers\Controller;
-use App\Domains\Schedule\DbScheduleRepository;
-use App\Domains\Schedule\Schedule;
+use App\Domains\Schedule\Contracts\ScheduleRepositoryInterface;
 
 class SchedulesController extends Controller
 {
     /**
+     * @param ScheduleRepositoryInterface $scheduleRepository
      * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
      */
-    public function index()
+    public function index(ScheduleRepositoryInterface $scheduleRepository)
     {
-        return (new DbScheduleRepository(new Schedule()))->getAll('schedules.id', 'asc');
+        return $scheduleRepository->getAll('schedules.id', 'asc');
     }
 }
